dd4c1cdcdb792bf499c5b278d56c62cf78b4d49f
“Lauryn <laurynmills111@gmail.com>
2020-07-19 21:09:35 +0200

    newgame button finished

--- app.js
+++ app.js
@@ -81,6 +81,13 @@ function init(){
 
     document.querySelector('#name-0').textContent = 'Player 1';
     document.querySelector('#name-1').textContent = 'Player 2';
+
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+
 }
 
 
